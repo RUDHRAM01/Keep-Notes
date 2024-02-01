b0ebc6efbefe9546c0e3aacae8b0b49cf17c7205
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-02-01 12:55:10 +0530

    adding create component

---
+++ src/app/post/create/create.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import { PostService } from '../post.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.css'
+})
+export class CreateComponent {
+  form!: FormGroup;
+  constructor(public postService:PostService,private router:Router) { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      body: new FormControl('', [Validators.required]),
+    })
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  submit() {
+    console.log(this.form.value);
+    this.postService.create(this.form.value).subscribe(res => {
+      console.log('Post created successfully!');
+      this.router.navigateByUrl('post/index');
+    })
+  }
+}
